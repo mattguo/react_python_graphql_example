465b11f12c50646dd517d2eda257619f18c1d435
Matt Guo <matt@mattguo.com>
2025-09-25 14:15:53 -0700

    rename project to react_python_graphql_example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import FilmList from './FilmList';
-import { Container, Group, Button, Image, Stack } from '@mantine/core';
+import { Stack } from '@mantine/core';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Stack gap="xl">
       <FilmList />
-      <Container size="lg">
-        <Group justify="center" align="center" gap="md" wrap="nowrap">
-          <Image
-            src={reactLogo}
-            alt="React logo"
-            w={50}
-            h={50}
-            style={{ cursor: 'help' }}
-          />
-          <Button
-            onClick={() => setCount(count => count + 1)}
-            size="md"
-            variant="filled"
-          >
-            count is {count}
-          </Button>
-        </Group>
-      </Container>
     </Stack>
   );
 }
